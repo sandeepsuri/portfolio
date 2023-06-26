e73c7dbd14192478e7286b318ce4eb0a7edc49d2
Sandeep Suri <35242894+sandeepsuri@users.noreply.github.com>
2023-06-25 21:04:54 -0400

    feat: skills section updated

--- portfolio/src/components/skills.tsx
+++ portfolio/src/components/skills.tsx
@@ -13,17 +13,42 @@ import Tailwind from '../assets/tailwind-css.png'
 
 export const Skills = () => {
     return (
-        <div name='skills' className='bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black'>
-            <div>
+        <div name='skills' className='w-full h-screen bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black text-gray-300'>
+            <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
                 <div>
-                    <p>My Full-Stack Experience</p>
-                    <p>Along with other tools I'm experienced in</p>
+                    <p className='text-4xl font-bold inline border-b-4'>My Full-Stack Experience</p>
+                    <p className='py-4'>+ other tools I'm experienced in</p>
                 </div>
-            </div>
-            <div>
-                <div>
-                    <Image src={HTML5} alt='HTML5 Logo' />
-                    <p>HTML 5</p>
+   
+                <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+                    <div className='shadow-md rounded-lg shadow-[#67696b] hover:scale-110 duration-500'>
+                        <Image className='w-20 mx-auto' src={HTML5} alt='HTML5 Logo' />
+                        <p className='py-4'>HTML 5</p>
+                    </div>
+                    <div className='shadow-md rounded-lg shadow-[#67696b] hover:scale-110 duration-500'>
+                        <Image className='w-20 mx-auto' src={NodeJS} alt='Node.js Logo' />
+                        <p className='py-4'>Node JS</p>
+                    </div>
+                    <div className='shadow-md rounded-lg shadow-[#67696b] hover:scale-110 duration-500'>
+                        <Image className='w-20 mx-auto' src={ReactJS} alt='React Logo' />
+                        <p className='py-4'>React JS + React Native</p>
+                    </div>
+                    <div className='shadow-md rounded-lg shadow-[#67696b] hover:scale-110 duration-500'>
+                        <Image className='w-20 mx-auto color-white' src={Tailwind} alt='Tailwind Logo' />
+                        <p className='py-4'>Tailwind CSS</p>
+                    </div>
+                    <div className='shadow-md rounded-lg shadow-[#67696b] hover:scale-110 duration-500'>
+                        <Image className='w-20 mx-auto color-white' src={Google} alt='Google Logo' />
+                        <p className='py-4'>Google Cloud Platform</p>
+                    </div>
+                    <div className='shadow-md rounded-lg shadow-[#67696b] hover:scale-110 duration-500'>
+                        <Image className='w-20 mx-auto color-white' src={Github} alt='Github Logo' />
+                        <p className='py-4'>Github</p>
+                    </div>
+                    <div className='shadow-md rounded-lg shadow-[#67696b] hover:scale-110 duration-500'>
+                        <Image className='w-20 mx-auto color-white' src={Adobe} alt='Github Logo' />
+                        <p className='py-4'>Adobe Experience Platform</p>
+                    </div>
                 </div>
             </div>
         </div>
