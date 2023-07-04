86d6fb1c8097ddfd79c82138e5b265e8e3e61020
Sandeep Suri <35242894+sandeepsuri@users.noreply.github.com>
2023-07-04 13:29:27 -0400

    feat: new navbar styling and animation

--- portfolio/src/components/navbar.tsx
+++ portfolio/src/components/navbar.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 "use client"
 import React, { useState } from 'react'
 import { HiOutlineMail } from 'react-icons/hi'
@@ -5,6 +6,8 @@ import { BsFillPersonLinesFill } from 'react-icons/bs'
 import Profile from '../assets/profile.png'
 import Image from 'next/image'
 import { Link } from 'react-scroll'
+
+//Icons and Logos
 import { 
     FaBars, 
     FaTimes,
@@ -13,29 +16,50 @@ import {
     FaLink,
     FaGit
 } from 'react-icons/fa'
-
 import { 
     BiDetail,
     BiHomeAlt, 
     BiUser
 } from 'react-icons/bi'
-
 import { GiGamepadCross } from 'react-icons/gi'
 import { HiOutlineMailOpen } from 'react-icons/hi'
+import PSLogo from '../assets/logos/playstation.svg'
+import XboxLogo from '../assets/logos/xbox.svg'
+import NintendoLogo from '../assets/logos/nintendo.svg'
 
 export const Navbar = () => {
+    const [menuClick, setMenuClick] = useState(false)
+    const [logoCycle, setLogoCycle] = useState(0)
+    const logos = [PSLogo, XboxLogo, NintendoLogo]
+
+    const toggleMenu = () => {
+        setMenuClick(!menuClick)
+        if(!menuClick) {
+            const nextLogo = (logoCycle + 1) % logos.length
+            setLogoCycle(nextLogo)
+        }
+    }
+
     const [navbar, setNavbar] = useState(false)
     const hamburgerClick = () => setNavbar(!navbar)
 
     return(
         <div className="flex justify-center">
-            <button className='fixed w-max[400px] h-[80px] flex justify-between items-center px-4 my-10 bg-[#0a192f] text-gray-300 rounded-full' data-te-ripple-init data-te-ripple-color="light">
+            {/* <div className='fixed w-max[400px] h-[80px] flex justify-between items-center px-4 my-10 bg-[#0a192f] text-gray-300 rounded-full' data-te-ripple-init data-te-ripple-color="light"> */}
+            <div className={`fixed flex justify-between items-center px-4 my-10 transition-all duration-500 
+                ${ menuClick ? 'w-[400px] h-[80px]' : 'w-[80px] h-[80px]' }
+                bg-[#0a192f] text-gray-300 rounded-full cursor-pointer`}
+                onClick={toggleMenu}
+                style={{
+                    ...(menuClick ? {} : { backgroundImage: `url(${logos[logoCycle].src})`, backgroundSize: 'cover'})
+                }}
+            >
                 {/* Nav Menu - Desktop */}
+                {menuClick ? (
                 <ul className='hidden md:flex items-center flex-grow justify-center'>
                     <li className=''>
                         <Link to='home' smooth={true} duration={300}><BiHomeAlt size={30} /></Link>
                     </li>
-
                     <li>
                         <Link to='about' smooth={true} duration={300}><BiUser size={30} /></Link>
                     </li>
@@ -49,6 +73,7 @@ export const Navbar = () => {
                         <Link to='contact' smooth={true} duration={300}><HiOutlineMailOpen size={30} /></Link>
                     </li>
                 </ul>
+                ) : null}
 
                 {/* Nav Menu - Hamburger, Hide for now*/}
                 <div onClick={hamburgerClick} className='md:hidden z-10'>
@@ -109,7 +134,31 @@ export const Navbar = () => {
                         </div>
                     </ul>
                 </div>
-            </button>
+            </div>
         </div>
     )
 }
+
+    <div className="flex justify-center">
+            <div className='fixed w-max[400px] h-[80px] flex justify-between items-center px-4 my-10 bg-[#0a192f] text-gray-300 rounded-full' data-te-ripple-init data-te-ripple-color="light">
+                {/* Nav Menu - Desktop */}
+                <ul className='hidden md:flex items-center flex-grow justify-center'>
+                    <li className=''>
+                        <Link to='home' smooth={true} duration={300}><BiHomeAlt size={30} /></Link>
+                    </li>
+
+                    <li>
+                        <Link to='about' smooth={true} duration={300}><BiUser size={30} /></Link>
+                    </li>
+                    <li>
+                        <Link to='skills' smooth={true} duration={300}><BiDetail size={30} /></Link>
+                    </li>
+                    <li>
+                        <Link to='project' smooth={true} duration={300}><GiGamepadCross size={30} /></Link>
+                    </li>
+                    <li>
+                        <Link to='contact' smooth={true} duration={300}><HiOutlineMailOpen size={30} /></Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
